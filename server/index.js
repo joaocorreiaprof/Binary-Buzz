d12acb50a91397480f010e28541d4485adff62c4
João Correia <joaocorreiaprof@gmail.com>
2025-02-13 00:40:16 +0000

    Update modules

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -12,13 +11,22 @@ const chatRoutes = require("./routes/chatRoutes");
 
 //Middleware
 const auth = require("./middlewares/auth");
-app.use(cors()); //delete in production
 app.use(express.json());
 
 //App Uses
 app.use("/api/users", userRoutes);
 app.use("/api/groups", auth, groupRoutes);
 app.use("/api/chats", auth, chatRoutes);
+
+// Serve static files from the client/dist directory
+const clientBuildPath = path.join(__dirname, "../../client/dist"); // Adjust path for deployment
+app.use(express.static(clientBuildPath));
+
+// Handle all other routes with the frontend's index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(clientBuildPath, "index.html"));
+  console.log("Static files served from:", clientBuildPath);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
